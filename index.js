6180928f073d5d50505ae31855fb66328ca74469
pavancrazy007 <58016744+pavancrazy007@users.noreply.github.com>
2022-04-11 15:25:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require('express')
+
+const app = express()
+const mysql = require('mysql')
+let listOfUsers = []
+
+// for parsing json
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'sqluser', // change the user id
+    password: 'password', // cahnge the password
+    database: 'evstudios' // change the database name 
+})
+
+// create connection
+db.connect(function(err) {
+    if (!!err) {
+        console.log(err)
+    }else {
+        console.log('connection established')
+    }
+})
+
+// create database
+app.get('/createdb', (req, res) => {
+    let sql = "CREATE DATABASE evstudios"
+    db.query(sql,(err) => {
+        if (err) {
+            console.error('Data base error')
+        }
+        res.send("Database created")
+    })
+})
+
+// for create comapny table
+app.get('/createCompanyTable', (req, res) => {
+    let sql = `CREATE TABLE  
+    company(companyId int not null auto_increment primary key,
+        companyName VARCHAR(255));`
+    db.query(sql, (err) => {
+        if (err) {
+            res.send('Cannot create Table an Error Occured' + err.message)
+        }else{
+        res.send('company Table created')
+    }})
+})
+
+// for creating user table
+
+app.get('/createUserTable', (req, res) => {
+    let sql = `CREATE TABLE user(
+        userId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        userName VARCHAR(255),
+        email VARCHAR(265),
+        mobile int,
+        PASSWORD VARCHAR(255),
+        companyId int ,
+        CONSTRAINT FOREIGN KEY (companyId)
+        REFERENCES company(companyId)
+      );`
+
+    db.query(sql, (err) => {
+        if (err) {
+            res.send('An occured while creating the user table' + err.message)
+        } else {
+        res.send('user Table created')
+    }})
+})
+
+
+// to Insert data into the company table
+
+app.post('/users/insert', (req, res) => {
+    const {user_name,email,mobile, password,companyId} = req.body
+    const sql = `
+    INSERT INTO user(userName,email,mobile,password,companyId)
+    VALUES('${user_name}','${email}','${mobile}','${password}',' ${companyId}');
+    `
+
+    db.query(sql, (err) => {
+        if (err) {
+            res.send('an error occurred while Insering data into the user Table' + err.message )
+        } else{
+        res.send('data inserted into the table successfully')}
+    })
+})
+
+
+// for inserting data into the company table
+app.post('/company/insert' , (req, res) => {
+    const {companyName} = req.body
+    const sql = `INSERT INTO company(companyName) VALUES ('${companyName}');`
+
+    db.query(sql, (err) => {
+        if (err) {
+            res.send('An erro occured while Insertinfg data into the company table ' + err.message)
+        }
+        res.send('data inserted into the table successfully')
+    })
+
+})
+
+
+// function to get the user by the company Id
+
+app.get('/getUsers', (req, res) => {
+
+    const sql = `
+    SELECT 
+    *
+    FROM
+    company
+        INNER JOIN 
+            user
+                ON company.companyId = user.companyId;
+    `
+    db.query(sql, (err,results) => {
+        if (err) {
+            res.send('An error occured')
+        } else {
+            
+            res.send(results)
+        }
+    })
+
+})
+
+
+app.listen(3004,()=> {
+    console.log('listening on http://localhost')
+})
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+## In this project i was implemented the sql database for **user & Company** ##
+
+## npm start to run the server
+
+## If the database is not present
+    http://localhost:3004/createdb --> to create database
+
+## for Creating the Company Table
+    http://localhost:3004/createCompanyTable --> to crate the Company Table
+
+## for creating user table
+    http://localhost:3004/createUserTable --> to create th user table
+
+## for Inserting data Into user table
+    http://localhost:3004/users/insert -->  to insert data into table user
+
+## for Inserting data Into company table
+    http://localhost:3004/company/insert'-->  to insert data into comapny table
+
+## for getting users by the emply id
+    http://localhost:3004/getUsers --> to get the dat fo the users with particular comapny Id
+    
